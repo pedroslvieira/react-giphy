15ceefb650204c8e1df281f99151f9028e5d8507
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-05-05 21:06:15 +0200

    errors in boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       template: '!!html-loader!templates/index.html'
     })
   ],
-  devtool: 'sourcemap',
+  devtool: 'source-map',
   mode: "development",
   module: {
     rules: [
