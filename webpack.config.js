66dc7df78f1acdfd5277f08c30abef346f7d7ad9
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-05-05 23:13:12 +0200

    finally everything works

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       template: '!!html-loader!templates/index.html'
     })
   ],
-  devtool: 'source-map',
+  devtool: 'sourcemap',
   mode: "development",
   module: {
     rules: [
